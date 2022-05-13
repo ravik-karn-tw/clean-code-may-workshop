8f64a338697daf38d61a8e4824cdc8f77344a7bb
ravik-karn-tw <ravik@thoughtworks.com>
2022-05-13 18:04:48 +0530

    Day - 1

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -20,44 +20,39 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
-        for (Rental each : rentals) {
-            double thisAmount = 0;
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
-
+        for (Rental rental : rentals) {
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" +
-                    thisAmount + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + rental.getMovie().getTitle() + "\t" +
+                    rental.amount() + "\n";
         }
 
         //add footer lines result
-        result += "Amount owed is " + totalAmount + "\n";
-        result += "You earned " + frequentRenterPoints
+        result += "Amount owed is " + totalAmount() + "\n";
+        result += "You earned " + frequentRenterPoints()
                 + " frequent renter points";
         return result;
     }
+
+    private double totalAmount() {
+        double totalAmount = 0;
+        for (Rental rental : rentals) {
+            totalAmount += rental.amount();
+        }
+        return totalAmount;
+    }
+
+    private int frequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        for (Rental rental : rentals) {
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two day new release rental
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+                    &&
+                    rental.getDaysRented() > 1) frequentRenterPoints++;
+        }
+        return frequentRenterPoints;
+    }
+
 }
--- src/test/java/com/thoughtworks/movierental/CustomerTests.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTests.java
@@ -2,9 +2,21 @@ package com.thoughtworks.movierental;
 
 import org.junit.jupiter.api.Test;
 
-class CustomerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CustomerTests {
 
     @Test
-    public void test() {
+    public void shouldGenerateStatement() {
+        Customer customer = new Customer("Bob");
+        customer.addRental(new Rental(new Movie("movie-1", Movie.REGULAR), 2));
+        customer.addRental(new Rental(new Movie("movie-2", Movie.CHILDRENS), 6));
+        customer.addRental(new Rental(new Movie("movie-3", Movie.NEW_RELEASE), 5));
+        assertEquals("Rental Record for Bob\n" +
+                "\tmovie-1\t2.0\n" +
+                "\tmovie-2\t6.0\n" +
+                "\tmovie-3\t15.0\n" +
+                "Amount owed is 23.0\n" +
+                "You earned 4 frequent renter points", customer.statement());
     }
 }
